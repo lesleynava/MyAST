26b6d9e8d481b6229c43371572b26dabad587792
lesleynava <p239p866@wichita.edu>
2025-05-03 18:25:33 -0500

    updated html and program file

--- Program.fs
+++ Program.fs
@@ -1,15 +1,38 @@
+open Giraffe
 open System
 open Microsoft.AspNetCore.Builder
 open Microsoft.Extensions.Hosting
 
+open Parse
+open Fun
+
 [<EntryPoint>]
 let main args =
     let builder = WebApplication.CreateBuilder(args)
-    let app = builder.Build()
+    builder.Services.AddGiraffe() |> ignore
 
-    app.MapGet("/", Func<string>(fun () -> "Hello World!")) |> ignore
+    let app = builder.Build()
+    app.UseStaticFiles() |> ignore
+
+
+    // app.MapGet("/", Func<string>(fun () -> "Hello World!")) |> ignore
+    let webApp =
+        choose [
+            route "/" >=> htmlFile "./Views/index.html"
+            // your other routes
+        ]
+    app.UseGiraffe(webApp)
+
+    printfn "%A" Parse.e1
+
+    printfn "%A" Parse.e2
+
+    eval Parse.e1 [] |> printfn "%A"    
+    eval Parse.e2 [] |> printfn "%A"
+
+    print Parse.e1 |> printfn "%A"
+    print Parse.e2 |> printfn "%A"
 
     app.Run()
 
     0 // Exit code
-
